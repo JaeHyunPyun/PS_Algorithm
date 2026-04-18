078cdede4e8199074121c0ead7ba95adc9170b57
JaeHyunPyun <pyunjaehyun@gmail.com>
2026-04-18 21:26:32 +0900

    Title: 두 정수에 대한 연산값 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260418/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void Modify(int &a, int &b){
+    if(a > b){
+        a *=2;
+        b +=10;
+    } else {
+        b *=2;
+        a +=10;
+    }
+}
+
+int main() {
+    
+    int a, b;
+    cin >> a >> b;
+
+    Modify(a,b);
+    cout << a << " " << b;
+
+    return 0;
+}
