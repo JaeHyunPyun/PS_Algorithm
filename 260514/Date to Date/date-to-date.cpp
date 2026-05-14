ef61bc6f2cc8337d508f03f65cc2e5fe6c68a5d7
JaeHyunPyun <pyunjaehyun@gmail.com>
2026-05-14 22:58:41 +0900

    [Passed] Date to Date (8ms, 0MB)

--- 260514/Date to Date/date-to-date.cpp
+++ 260514/Date to Date/date-to-date.cpp
@@ -3,25 +3,27 @@ using namespace std;
 
 int daysOfMonth[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+int countDays(int m, int d){
+    int count = 0;
+
+    for(int i = 1; i < m; i++){
+        count += daysOfMonth[i];
+    }
+
+    count += d;
+
+    return count;
+}
+
 int main() {
     int m1, d1, m2, d2;
 
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int cnt = 0;
-
-    for (int i = m1 + 1; i < m2; i++){
-        cnt += daysOfMonth[i];
-    }
-
-    if(m1 != m2){
-        cnt += daysOfMonth[m1]  - d1 + d2 + 1;
-    } else {
-        cnt += d2 - d1 + 1;
-    }
-
-    
-    cout << cnt;
+    int firstDays = countDays(m1, d1);
+    int secondDays = countDays(m2, d2);
+        
+    cout << secondDays - firstDays + 1;
 
     return 0;
 }
