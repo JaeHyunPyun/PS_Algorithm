2948e1ac5e99ab2b2bb680a0ebee6081331a3645
JaeHyunPyun <pyunjaehyun@gmail.com>
2026-07-24 19:14:32 +0900

    [Passed] 연속되는 수 4 (2ms, 0MB)

---
+++ 260724/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_ARRAY_SIZE 1000
+
+using namespace std;
+
+int array1[MAX_ARRAY_SIZE];
+
+int main() {
+    int n;
+    int cursor = 0;
+    int max_cnt = 0;
+
+    cin >> n;
+
+    for (int i=0; i<n; i++){
+        cin >> array1[cursor++];
+    }
+
+    int cnt = 1;
+    for (int i=0; i<n; i++){
+        if(i==0 || array1[i]<=array1[i-1]){
+            max_cnt = max(cnt, max_cnt);
+            cnt = 1;
+        } else {
+            cnt++;
+        }
+    }
+    
+    max_cnt = max(cnt, max_cnt);
+
+    cout << max_cnt;
+
+    return 0;
+}
